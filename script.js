92ef9c002acac8f89795c2d56fa57e9d67ce91e4
Francis <167620859+francislame@users.noreply.github.com>
2025-08-31 00:22:16 -0500

    FIXED Intro-main transition

--- script.js
+++ script.js
@@ -3,21 +3,35 @@ window.addEventListener("load", () => {
   const introDogPrints1 = document.getElementById("intro-dog-prints1");
   const main = document.getElementById("main");
 
-  // Optional scale effect
-  setTimeout(() => {
-    introDogPrints1.style.transform = "scale(2.5)";
+  setTimeout(() => 
+  {
+    introDogPrints1.style.transform = "scale(2)";
   }, 300);
 
   // Wait for slide animation to finish
-  introDogPrints1.addEventListener("animationend", () => {
+  introDogPrints1.addEventListener("animationend", () => 
+    {
     intro.style.opacity = "0";
     intro.style.transition = "opacity 1s ease";
 
     setTimeout(() => {
       intro.style.display = "none";
+
       main.style.display = "block";
+      setTimeout(() => 
+        {
+        main.style.opacity = "1"; // fade in
+        document.body.style.overflow = "auto";
+        }, 50);
+
       main.style.transition = "opacity 1s ease";
-      main.style.opacity = "1"; // fade in main content
+
+      main.style.opacity = "0";
+      setTimeout(() => 
+        {
+        main.style.display = "none";
+        }, 3000);
+
       document.body.style.overflow = "auto"; // allow scrolling
     }, 1000); // matches the opacity transition
   });
