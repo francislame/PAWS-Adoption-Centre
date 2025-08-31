ae9ee4cadbed89e268351faf33e13f1e46bd0657
Francis <167620859+francislame@users.noreply.github.com>
2025-08-31 13:58:36 -0500

    ADDED MAIN MENU
    FIXED intro to main menu animations and transition
    ADDED Main Menu Labels

--- script.js
+++ script.js
@@ -2,37 +2,44 @@ window.addEventListener("load", () => {
   const intro = document.getElementById("intro");
   const introDogPrints1 = document.getElementById("intro-dog-prints1");
   const main = document.getElementById("main");
+  const menu = document.getElementById("menu");
 
-  setTimeout(() => 
+  //Dogprint intro size
+  setTimeout(() =>
   {
     introDogPrints1.style.transform = "scale(2)";
   }, 300);
 
-  // Wait for slide animation to finish
+  // This will wait for the dog print animation to complete
   introDogPrints1.addEventListener("animationend", () => 
-    {
+  {
     intro.style.opacity = "0";
     intro.style.transition = "opacity 1s ease";
-
-    setTimeout(() => {
+    setTimeout(() => 
+    {
       intro.style.display = "none";
-
+      
+      
+      //Main logo animation start
       main.style.display = "block";
       setTimeout(() => 
-        {
-        main.style.opacity = "1"; // fade in
+      {
+        main.style.opacity = "1";
         document.body.style.overflow = "auto";
-        }, 50);
+      }, 50);
 
       main.style.transition = "opacity 1s ease";
 
       main.style.opacity = "0";
       setTimeout(() => 
-        {
+      {
         main.style.display = "none";
-        }, 3000);
+      }, 3000);
 
-      document.body.style.overflow = "auto"; // allow scrolling
-    }, 1000); // matches the opacity transition
+      menu.style.display = "block"; 
+        setTimeout(() => {
+          menu.style.opacity = "1";
+        }, 3000);
+    }, 1000);
   });
 });
